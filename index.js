5dc99d15013b76954e2e804eeac3baa3c19e0292
Kevin Coyner <rustybear@gmail.com>
2017-12-01 23:16:52 -0500

    Changed authentication code to firebase-admin.

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
+/*
+ * index.js
+ */
+
 var express = require('express');
 var app = express();
 var request = require('request');
 var firebase = require('firebase');
-var cors = require('cors')
+var cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 require('firebase/database');
 
-var firebase_cred = {
-    apiKey: "AIzaSyAyscBTLhOFfz3rWvxCcg9Woyj5zJAKy9U",
-    authDomain: "ers-dispatch.firebaseapp.com",
-    databaseURL: "https://ers-dispatch.firebaseio.com",
-    projectId: "ers-dispatch",
-    storageBucket: "ers-dispatch.appspot.com",
-    messagingSenderId: "412226656783"
-}
-firebase.initializeApp(firebase_cred);
-
-firebase.auth().signInWithEmailAndPassword('emergency.response.solutions1@gmail.com', 'password')
-.then(function(){
-  firebase.auth().onAuthStateChanged(function(user) {
-    console.log('Logged in as: ', user.email);
-  });
-})
-.catch(function(error) {
-  console.log(error);
-});
-
+const admin = require('firebase-admin');
+const serviceAccount = require('./key/ers-dispatch-firebase-adminsdk-08k8q-3c9e3d13f9');
+const firebaseCred = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://ers-dispatch.firebaseio.com',
+};
+admin.initializeApp(firebaseCred);
+const db = admin.database();
 
 app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
-  console.log('Running on port', app.get('port'));
-});
-
 app.get('/api/', (req, res) => {
-  if (req.query.code){
+  if (req.query.code) {
     var code = req.query.code;
-    firebase.database().ref(`/ersDispatches/${code}`).once('value')
-    .then(function(snapshot){
-      if(snapshot) {
-        console.log(snapshot.val());
-        res.send(JSON.stringify(snapshot.val()));
-      }
-    })
-    .catch(function(err){
-      res.send(err);
-    });
+    db.ref(`/ersDispatches/${code}`).once('value')
+      .then(function(snapshot) {
+        if (snapshot) {
+          console.log(snapshot.val());
+          res.send(JSON.stringify(snapshot.val()));
+        }
+      })
+      .catch(function(err) {
+        res.send(err);
+      });
   } else {
     res.send('Please include key');
   }
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Running on port', app.get('port'));
 });
